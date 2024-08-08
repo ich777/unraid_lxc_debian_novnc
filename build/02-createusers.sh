c8b513b5992f98c7de325b8df8a4b0f1c2185fc3
Christoph Hummer <christophhummer@gmail.com>
2024-08-08 18:00:34 +0200

    Update 02-createusers.sh

--- build/02-createusers.sh
+++ build/02-createusers.sh
@@ -2,3 +2,6 @@
 echo -e 'Unraid\nUnraid' | passwd root
 useradd -rm debian -s /bin/bash
 echo -e 'debian\ndebian' | passwd debian
+
+echo -e "\ncd /root\nsu - debian" >> /root/.bashrc
+hostnamectl set-hostname novnc
